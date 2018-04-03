74cce110da8f639bc7b9973f4f007ba11aa274b4
micky4966 <30958246+micky4966@users.noreply.github.com>
2018-04-03 11:11:31 +0200

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install FFmpeg"
+echo "Install FFmpeg and FFprobe"
 wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 /bin/mkdir /usr/src/ffmpeg
 tar -xJf ffmpeg-release-64bit-static.tar.xz -C /usr/src/ffmpeg
@@ -8,3 +8,12 @@ tar -xJf ffmpeg-release-64bit-static.tar.xz -C /usr/src/ffmpeg
 /bin/cp /usr/src/ffmpeg/ffmpeg*/ffprobe /usr/bin/ffprobe
 chmod 755 /usr/bin/ffmpeg
 chmod 755 /usr/bin/ffprobe
+
+echo "******************************************************************************************** \n";
+echo "Your panel is installed !!!\n";
+echo "Login page: 'http://{$srv_ip}:8000' \n";
+echo "With: \n";
+echo "Username: admin \n";
+echo "Password: admin \n";
+echo "IMPORTANT: After you logged in, go to Admin and change password. \n";
+echo "******************************************************************************************** \n";
