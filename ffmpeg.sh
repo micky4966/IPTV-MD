786ebcffa7e11beadbd6624d30e06da7cb487f55
micky4966 <30958246+micky4966@users.noreply.github.com>
2019-06-17 21:34:34 +0200

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Install FFmpeg and FFprobe"
-wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 /bin/mkdir /usr/src/ffmpeg
-tar -xJf ffmpeg-release-64bit-static.tar.xz -C /usr/src/ffmpeg
+tar -xJf ffmpeg-release-amd64-static.tar.xz -C /usr/src/ffmpeg
 /bin/cp /usr/src/ffmpeg/ffmpeg*/ffmpeg  /usr/bin/ffmpeg
 /bin/cp /usr/src/ffmpeg/ffmpeg*/ffprobe /usr/bin/ffprobe
 chmod 755 /usr/bin/ffmpeg
